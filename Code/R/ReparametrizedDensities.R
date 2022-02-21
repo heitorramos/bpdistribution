d292cff79c0720eca2bae508cbc31f0d8b9cbf36
Alejandro C. Frery <acfrery@gmail.com>
2022-02-22 10:36:34 +1300

    Two code files

---
+++ Code/R/ReparametrizedDensities.R
@@ -0,0 +1,22 @@
+require(nleqslv)
+
+dWeibullMeanVar <- function(x, mean, variance) {
+  
+}
+
+dBetaMeanVar <- function(x, mean, variance) {
+  
+}
+
+dLognormMeanVar <- function(x, mean, variance) {
+  
+}
+
+dGammaMeanVar <- function(x, mean, variance) {
+  
+}
+
+dHyperbolicMeanVar <- function(x, mean, variance) {
+  
+}
+
